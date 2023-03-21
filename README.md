1824024ad7f87a2f116f166508d17179cf329bab
Ming-der Wang <mingderwang@gmail.com>
2023-03-21 11:27:54 +0800

    release

---
+++ README.md
@@ -0,0 +1,7 @@
+# warning
+
+only support in v2 not v3
+
+## codegen
+
+This code is generate by chatGPT
--- background.js
+++ background.js
@@ -1,12 +1,22 @@
-chrome.webRequest.onCompleted.addListener(function(details) {
-  fetch(details.url).then(function(response) {
-    const reader = response.body.getReader();
-    const writer = new WritableStream({
-      write(chunk) {
-        console.log(chunk.toString());
-      }
+chrome.scripting.registerContentScript({
+  target: { allFrames: true },
+  function: function(details) {
+    fetch(details.url).then(function(response) {
+      const reader = response.body.getReader();
+      const writer = new WritableStream({
+        write(chunk) {
+          console.log(chunk.toString());
+        }
+      });
+      reader.pipeTo(writer);
     });
-    reader.pipeTo(writer);
+  }
+});
+
+chrome.webRequest.onCompleted.addListener(function(details) {
+  chrome.scripting.executeScript({
+    target: { tabId: details.tabId },
+    function: function(details) {},
+    args: [details]
   });
 }, { urls: ["<all_urls>"] });
-
